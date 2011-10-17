43c49124654f083dfc4e65e66c2e272b8eb1cd0d
Bryan Madsen <bryan@instructure.com>
2011-10-17 13:33:59 -0600

    Add hudson_setup.sh for a hudson build of this plugin
    Change-Id: I79f662e5b192b625aeffdfc885f6492d31a9327a
    Reviewed-on: https://gerrit.instructure.com/6243
    Tested-by: Hudson <hudson@instructure.com>
    Reviewed-by: Bracken Mosbacker <bracken@instructure.com>

---
+++ spec_canvas/hudson_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+git clone "ssh://hudson@gerrit.instructure.com:29418/migration_tool.git" "vendor/plugins/migration_tool"
+
+vendor/plugins/migration_tool/spec_canvas/hudson_setup.sh
+
+#qti_migration_tool is installed by migration_tool hudson_setup.sh
+set +e
+rm -r vendor/qti_migration_tool
+set -e
