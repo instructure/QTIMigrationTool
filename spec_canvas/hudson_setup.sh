86246c0e834e00842794ac48bc6b6285702a7fd7
Cody Cutrer <cody@instructure.com>
2015-01-15 10:31:39 -0700

    prepare for migration_tool gemification
    Change-Id: I71996673c7e09ee2ba625ec569ef2037ceabb8d6
    Reviewed-on: https://gerrit.instructure.com/47235
    Tested-by: Jenkins <jenkins@instructure.com>
    Reviewed-by: Nick Cloward <ncloward@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- spec_canvas/hudson_setup.sh
+++ spec_canvas/hudson_setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-git clone "$REPO/migration_tool.git" "vendor/plugins/migration_tool"
 
-vendor/plugins/migration_tool/spec_canvas/hudson_setup.sh
+TARGET=vendor/plugins/migration_tool
+
+git clone "$REPO/migration_tool.git" $TARGET
+
+if [ -f vendor/plugins/migration_tool/migration_tool.gemspec ]; then
+  mv vendor/plugins/migration_tool gems/plugins/migration_tool
+  TARGET=gems/plugins/migration_tool
+fi
+
+$TARGET/spec_canvas/hudson_setup.sh
 
 #qti_migration_tool is installed by migration_tool hudson_setup.sh
 set +e
